db77702592dadf2d30904010bab8434299365d2a
CristianBrogni <103286040+CristianBrogni@users.noreply.github.com>
2023-03-06 16:19:10 -0300

    Add files via upload
    Condição adicionada ao PertemceFibonacci.java impedindo valores menores iguais a zero

--- CodigoParaTesteLogico/src/PertenceFibonacci.java
+++ CodigoParaTesteLogico/src/PertenceFibonacci.java
@@ -1,12 +1,16 @@
 public class PertenceFibonacci {
     public static void main(String[] args) {
-        System.out.println(pertence(233));
+        System.out.println(pertence(1));
     }
 
     public static String pertence(int num){
         int result = 0;
         int num1 = 0;
         int num2 = 1;
+
+        if (num <= 0){
+            return "O numero deve ser positivo e maior que zero";
+        }
 
         for (int i = 1; i <= num; i++){
             result = num1 + num2;
